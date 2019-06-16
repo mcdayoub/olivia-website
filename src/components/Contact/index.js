f4c04eb6b879ea84a3d18315b3593289bd236f9e
Miles <mcdayoub@gmail.com>
2019-06-16 18:34:44 -0400

    initiial

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -71,21 +71,6 @@ class Contact extends Component {
     this.setState({
       buttonText: '...sending'
     });
-
-    let data = {
-      name: this.state.name,
-      email: this.state.email,
-      message: this.state.message
-    };
-
-    axios
-      .post('API_URI', data)
-      .then(res => {
-        this.setState({ sent: true }, this.resetForm());
-      })
-      .catch(() => {
-        console.log('Message not sent');
-      });
   };
   resetForm = () => {
     this.setState({
@@ -130,7 +115,7 @@ class Contact extends Component {
             </div>
             <form
               className="contact-form"
-              action="POST"
+              method="POST"
               data-netlify="true"
               onSubmit={e => this.formSubmit(e)}
             >
